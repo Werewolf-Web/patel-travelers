f4bc088a955c4f2aaf24e5df0151eadc26d4d60f
Werewolf-Web <professor.sujal@gmail.com>
2025-08-31 16:50:56 +0530

    31 arrange memori card

--- src/component/Memories/Memories.jsx
+++ src/component/Memories/Memories.jsx
@@ -3,11 +3,13 @@ import memoriesCSS from './../Memories/Memories.module.css';
 
 import MemoriesImg1 from './../../assets/travelport1.jpg';
 import MemoriesImg2 from './../../assets/travelport2.jpg';
-import MemoriesImg3 from './../../assets/travelport3.jpg';
+import MemoriesImg3 from './../../assets/travelport3.jpeg';
 import MemoriesImg4 from './../../assets/travelport4.jpg';
 import MemoriesImg5 from './../../assets/travelport5.jpg';
-
+import MemoriesImg6 from './../../assets/travelport6.jpeg';
 import MemoriesImg7 from './../../assets/travelport7.jpg';
+import MemoriesImg8 from './../../assets/travelport8.jpeg';
+import MemoriesImg9 from './../../assets/travelport9.jpeg';
 function Memories() {
     return (
         <div className={`${memoriesCSS.Memories_wrapper} section`}>
@@ -19,7 +21,7 @@ function Memories() {
                     <a href="#">View tours</a>
                 </div>
             </div>
-                <div className={memoriesCSS.MemoriesCard}>
+            <div className={memoriesCSS.MemoriesCard}>
                 <img src={MemoriesImg2} alt="Memories Img 2" />
 
                 <div className={memoriesCSS.content}>
@@ -27,14 +29,14 @@ function Memories() {
                     <a href="#">View tours</a>
                 </div>
             </div>
-                <div className={memoriesCSS.MemoriesCard}>
+            <div className={memoriesCSS.MemoriesCard}>
                 <img src={MemoriesImg3} alt="Memories Img 3" />
 
                 <div className={memoriesCSS.content}>
                     <h3>Undiscover Tourse</h3>
                     <a href="#">View tours</a>
                 </div>
-            </div>    
+            </div>
             <div className={memoriesCSS.MemoriesCard}>
                 <img src={MemoriesImg4} alt="Memories Img 4" />
 
@@ -42,7 +44,7 @@ function Memories() {
                     <h3>Let Our Experts,<br />Plan Your Trip</h3>
                     <button href="#">View tours</button>
                 </div>
-            </div>    
+            </div>
             <div className={memoriesCSS.MemoriesCard}>
                 <img src={MemoriesImg5} alt="Memories Img 5" />
 
@@ -50,10 +52,32 @@ function Memories() {
                     <h3>Affordable Dreams</h3>
                     <a href="#">View tours</a>
                 </div>
-            </div>    
-            
-               <div className={memoriesCSS.MemoriesCard}>
+            </div>
+            <div className={memoriesCSS.MemoriesCard}>
+                <img src={MemoriesImg6} alt="Memories Img 6" />
+
+                <div className={memoriesCSS.content}>
+                    <h3>Affordable Dreams</h3>
+                    <a href="#">View tours</a>
+                </div>
+            </div>
+            <div className={memoriesCSS.MemoriesCard}>
                 <img src={MemoriesImg7} alt="Memories Img 7" />
+
+                <div className={memoriesCSS.content}>
+                    <h3>Affordable Dreams</h3>
+                    <a href="#">View tours</a>
+                </div>
+            </div>
+             <div className={memoriesCSS.MemoriesCard}>
+                <img src={MemoriesImg8} alt="Memories Img 8" />
+
+                <div className={memoriesCSS.content}>
+                    <h3>Affordable Dreams</h3>
+                    <a href="#">View tours</a>
+                </div>
+            </div> <div className={memoriesCSS.MemoriesCard}>
+                <img src={MemoriesImg9} alt="Memories Img 9" />
 
                 <div className={memoriesCSS.content}>
                     <h3>Affordable Dreams</h3>
